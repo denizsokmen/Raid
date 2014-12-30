b9fe0b750847a9e561552e081f08df416b32a8e0
denizsokmen <deniz.sokmen@ozu.edu.tr>
2014-12-30 13:48:50 +0200

    Project, bugreport

---
+++ Raid/BugReport.swift
@@ -0,0 +1,22 @@
+//
+//  BugReport.swift
+//  Raid
+//
+//  Created by student7 on 30/12/14.
+//  Copyright (c) 2014 student7. All rights reserved.
+//
+
+import Foundation
+
+class BugReport {
+    var description: String!
+    var title: String!
+    var priority: Int!
+    
+    
+    init() {
+        
+    }
+    
+    
+}
---
+++ Raid/Project.swift
@@ -0,0 +1,16 @@
+//
+//  Project.swift
+//  Raid
+//
+//  Created by student7 on 30/12/14.
+//  Copyright (c) 2014 student7. All rights reserved.
+//
+
+import Foundation
+
+class Project {
+    var bugs: [BugReport]!
+    var name: String!
+    
+    
+}
